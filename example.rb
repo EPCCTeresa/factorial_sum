42b6f446c203751f98f286393f875766ff1e250d
Teresa Simancas Fernández <teresasimancas@MacBook-Air-de-Teresa.local>
2021-06-07 12:03:26 +0200

    Add loop to calculate the factorial sum of all the numbers contained in a range and accumulate all of them

--- example.rb
+++ example.rb
@@ -1,2 +1,10 @@
 limit = ARGV[0].to_i
-puts "La suma factorial es #{limit.downto(1).inject(0) { |acc, x| acc + x }}"
+total = 0
+
+limit.downto(1).each do |i|
+    iteration_sum = i.downto(1).inject(0) { |acc, x| acc + x }
+    total = total+iteration_sum
+    puts "La suma factorial de #{i} es #{iteration_sum}"
+end
+
+puts "La suma de todos los factoriales en el rango #{limit} a 1 es: #{total}"
